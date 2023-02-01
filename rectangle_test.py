b2e8ad2b2c3ec1cd0319b38d26a79c04f6519467
Marc Canizares <marc@mobilabsolutions.com>
2023-02-01 10:24:00 +0100

    New changes

--- rectangle_test.py
+++ rectangle_test.py
@@ -15,6 +15,10 @@ class RectangleTest(unittest.TestCase):
         rectangle = rectangle_measures.Rectangle(2, 3)
         self.assertEqual(rectangle.retrieve_dimensions(), (2, 3), "Incorrect rectangle")
 
+    def test_diagonal(self):
+        rectangle = rectangle_measures.Rectangle(5, 12)
+        self.assertEqual(rectangle.get_diagonal(), 13, "Incorrect rectangle")
+
     def test_negative(self):
         rectangle = rectangle_measures.Rectangle(2, 3)
         self.assertGreaterEqual((rectangle.width and rectangle.height), 0, "Incorrect rectangle, can't be negative")
